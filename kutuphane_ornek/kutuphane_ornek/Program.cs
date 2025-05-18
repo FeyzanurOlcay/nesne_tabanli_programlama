07f7412dc9641f1d2d46ad12cdfed2163839ea47
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-18 12:11:14 +0300

    Initial commit

---
+++ kutuphane_ornek/kutuphane_ornek/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kutuphane_ornek
+{
+    class Kitap
+    {
+        public string isim;
+        public string yazar;
+        public string isbn;
+        public int sayfa;
+
+        public Kitap(string isim, string yazar, string isbn, int sayfa)
+        {
+            this.isim = isim;
+            this.yazar = yazar;
+            this.isbn = isbn;
+            this.sayfa = sayfa;
+                
+        }
+
+        public void bilgiGoster()
+        {
+            Console.WriteLine($"Kitap adı: {isim}");
+            Console.WriteLine($"Kitap yazarı: {yazar}");
+            Console.WriteLine($"Kitap isbn: {isbn}");
+            Console.WriteLine($"Kitap sayfa sayısı: {sayfa}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kitap kitap1 = new Kitap("Sefiller", "Victor Hugo", "123345", 250);
+            Kitap kitap2 = new Kitap("Kürk mantolu madonna","Sabahattin Ali", "122222", 200);
+
+            kitap1.bilgiGoster();
+            kitap2.bilgiGoster();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ogrenci_sistem/ogrenci_sistem/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ogrenci_sistem
+{
+    public class Ogrenci
+    {
+        private int no;
+        private string isim;
+        private string soyisim;
+        private string okulAdi;
+        private int vize1;
+        private int vize2;
+        private int final;
+
+        public Ogrenci(int _no, string _ad, string _soyad, string _okulAdi, int _vize1, int _vize2, int _final)
+        {
+            no = _no;
+            isim = _ad;
+            soyisim = _soyad;
+            okulAdi = _okulAdi;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+        }
+
+        public void bilgiGoster()
+        {
+            Console.WriteLine("Öğrenci numarası :" + no);
+            Console.WriteLine("Öğrenci isim :" + isim);
+            Console.WriteLine("Öğrenci soyisim :" + soyisim);
+            Console.WriteLine("Öğrenci okulu :" + okulAdi);
+            Console.WriteLine("Öğrenci vize1 :" + vize1);
+            Console.WriteLine("Öğrenci vize2 :" + vize2);
+            Console.WriteLine("Öğrenci final :" + final);
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrencinin okuduğu okul : " + okulAdi);
+        }
+
+        public double ortalama()
+        {
+            double ort = (vize1 * 0.2) + (vize2 * 0.2) + (final * 0.6);
+            return ort;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool kontrol = true;
+            Ogrenci ogr1 = new Ogrenci(123, "Nur", "Olcay", "AHBV", 75, 68, 89);
+
+            Console.WriteLine("Hoşgeldiniz...");
+
+            while (kontrol)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Bir seçim yapınız:");
+                islem();
+                Console.Write("seçiminiz: ");
+                string secim = Console.ReadLine();
+
+                switch (secim)
+                {
+                    case "1":
+                        ogr1.bilgiGoster();
+                        break;
+                    case "2":
+                        ogr1.okulGetir();
+                        break;
+                    case "3":
+                        double ort = ogr1.ortalama();
+                        Console.WriteLine("Öğrencinin ortalaması :"+ ort);
+                        break;
+                    case "4":
+                        kontrol = false;
+                        break;
+                    default:
+                        Console.WriteLine("Geçersiz seçim! Tekrar deneyiniz.");
+                        break;
+                }
+            }
+
+        }
+        static void islem()
+        {
+            Console.WriteLine(" 1 - Öğrenci bilgisini göster");
+            Console.WriteLine(" 2 - Öğrenci okulunu göster");
+            Console.WriteLine(" 3 - Öğrenci ortalamasını göster");
+            Console.WriteLine(" 4 - Çıkış yap");
+        }
+    }
+}
