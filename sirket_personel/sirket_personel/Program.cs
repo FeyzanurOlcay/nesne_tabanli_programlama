992f9db320d7618ea7dff010501d0d6a780d29c0
FeyzanurOlcay <nurolcay000@gmail.com>
2025-05-18 12:08:07 +0300

    Initial commit

---
+++ sirket_personel/sirket_personel/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sirket_personel
+{
+    class personel
+    {
+        public int id;
+        public string isim;
+        public string pozisyon;
+        public string departman;
+        public int maas;
+
+        public personel(int id, string isim, string pozisyon, string departman, int maas)
+        {
+            this.id = id;
+            this.isim = isim;
+            this.pozisyon = pozisyon;
+            this.departman = departman;
+            this.maas = maas;
+
+        }
+
+        public void bilgiGoster()
+        {
+            Console.WriteLine("PERSONEL BİLGİLERİ");
+            Console.WriteLine($"ID : {id}");
+            Console.WriteLine($"ISIM : {isim}");
+            Console.WriteLine($"POZİSYON : {pozisyon}");
+            Console.WriteLine($"DEPARTMAN : {departman}");
+            Console.WriteLine($"MAAS : {maas}");
+        }
+
+        public class Sirket
+        {
+            List<personel> personeller = new List<personel>();
+
+            public void personeEkle(personel yeniPersonel)
+            {
+                personeller.Add(yeniPersonel);
+                Console.WriteLine($"{yeniPersonel.isim} şirkete eklendi.");
+            }
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ sirket_personel/sirket_personel/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("sirket_personel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sirket_personel")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("86cbf210-aecd-4b19-9c01-1adebf2d9760")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
